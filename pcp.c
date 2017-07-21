853d8fcb0334b1dbac0386360cc64084240c147d
iml1111 <root@localhost.localdomain>
2017-07-21 10:50:48 +0900

    fix all,2nd HW

--- pcp.c
+++ pcp.c
@@ -8,82 +8,75 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pcap/pcap.h>
 int main(int argc, char *argv[])
 {
 	pcap_t *handle;			/* Session handle */
 	char *dev;			/* The device to sniff on */
 	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-	struct bpf_program fp;		/* The compiled filter */
-	char filter_exp[] = "port 80";	/* The filter expression */
-	bpf_u_int32 mask;		/* Our netmask */
-	bpf_u_int32 net;		/* Our IP */
 	struct pcap_pkthdr header;	/* The header that pcap gives us */
 	const u_char *packet;		/* The actual packet */
 	
-	int i,j;
+	int i,j,nextck;
+	char buf[20]={0};
+	char buf2[20]={0};
 	struct ip *iph;
 	struct tcphdr *tcph;
 	struct ether_header *ethh;
+	
+	if(argc == 1){
+		printf("No Interface\n");
+		return -1;
+	}	
 
 	/* Define the device */
-	dev = pcap_lookupdev(errbuf);
-	if (dev == NULL) {
-		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-		return(2);
-	}
-	/* Find the properties for the device */
-	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-		net = 0;
-		mask = 0;
-	}
+	dev = argv[1];
+	
 	/* Open the session in promiscuous mode */
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 	if (handle == NULL) {
 		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
 		return(2);
 	}
-	/* Compile and apply the filter */
-	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return(2);
-	}
-	if (pcap_setfilter(handle, &fp) == -1) {
-		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-		return(2);
-	}
 	/* Grab a packet */
 	while(1){
-		packet = pcap_next(handle, &header);
+		nextck = pcap_next_ex(handle, &header,&packet);
+		if(nextck == 0) continue;
+		else if(nextck == -1 || nextck == -2) break;		
 		
-		ethh =(struct ether_header *)packet;
-		packet +=sizeof(struct ether_header);
-		
-		iph =(struct ip *)packet;
-		packet += sizeof(struct ip);
-
-		tcph =(struct tcphdr *)packet;
-		packet += sizeof(struct tcphdr);
 		printf("=============================\n");
+		ethh =(struct ether_header *)packet;
 		printf("eth.smac = "); for(j=0;j<6;j++)printf("%02x:",ethh->ether_shost[j]); printf("\n");
-		printf("eth.dmac = "); for(j=0;j<6;j++)printf("%02x:",ethh->ether_dhost[j]); printf("\n");
-		
-		printf("ip.sip = %s\n",inet_ntoa(iph->ip_src));
-		printf("ip.dip = %s\n",inet_ntoa(iph->ip_dst));
+                printf("eth.dmac = "); for(j=0;j<6;j++)printf("%02x:",ethh->ether_dhost[j]); printf("\n");
 
-		printf("tcp.sport = %d\n",ntohs(tcph->th_sport));
-		printf("tcp.dport = %d\n",ntohs(tcph->th_dport));
+		packet +=14;
+	       
+		if( ntohs(ethh->ether_type) == ETHERTYPE_IP ){	
+			iph =(struct ip *)packet;
+        	        inet_ntop(AF_INET,&(iph->ip_src),buf,sizeof(buf));
+	                inet_ntop(AF_INET,&(iph->ip_dst),buf2,sizeof(buf2));
+			printf("ip.sip = %s\n",buf);
+			printf("ip.dip = %s\n",buf2);
+			packet += (iph->ip_hl * 4);
 		
-		printf("data:\n");
-		for(j=0;j<(header.len)-sizeof(struct ether_header)-
-					sizeof(struct ip)-sizeof(struct tcphdr);j++){
-			printf("%02x ",*packet);
-			packet++;
+			if(iph->ip_p == IPPROTO_TCP){
+				tcph =(struct tcphdr *)packet;
+				printf("tcp.sport = %d\n",ntohs(tcph->th_sport));
+				printf("tcp.dport = %d\n",ntohs(tcph->th_dport));
+				packet += (tcph->th_off * 4);
 
-			if(j % 20 == 0 && j!=0)
-			printf("\n");
+				printf("data:\n");
+                        	for(j=1;j<=ntohs(iph->ip_len) - (tcph->th_off * 4) - (iph->ip_hl * 4) - 14;j++){
+                        	printf("%02x ",*packet);
+                        	packet++;
+
+				if(j % 20 == 0)
+        	                printf("\n");
+	                        }
+			}
 		}
-		printf("\n=============================\n");
+                printf("\n==============================\n");
+
 	}
 	pcap_close(handle);	
 	return(0);
