60f561c0630d7c22d72d9c26065d21e2848c4f65
iml1111 <root@localhost.localdomain>
2017-07-21 13:19:03 +0900

    port filtering add

--- pcp.c
+++ pcp.c
@@ -17,9 +17,9 @@ int main(int argc, char *argv[])
 	struct pcap_pkthdr header;	/* The header that pcap gives us */
 	const u_char *packet;		/* The actual packet */
 	
-	int i,j,nextck;
-	char buf[20]={0};
-	char buf2[20]={0};
+	int i,j,nextck,sport,dport;
+	char sip[20]={0};
+	char dip[20]={0};
 	struct ip *iph;
 	struct tcphdr *tcph;
 	struct ether_header *ethh;
@@ -43,40 +43,43 @@ int main(int argc, char *argv[])
 		nextck = pcap_next_ex(handle, &header,&packet);
 		if(nextck == 0) continue;
 		else if(nextck == -1 || nextck == -2) break;		
-		
-		printf("=============================\n");
+	
 		ethh =(struct ether_header *)packet;
-		printf("eth.smac = "); for(j=0;j<6;j++)printf("%02x:",ethh->ether_shost[j]); printf("\n");
-                printf("eth.dmac = "); for(j=0;j<6;j++)printf("%02x:",ethh->ether_dhost[j]); printf("\n");
-
 		packet +=14;
 	       
 		if( ntohs(ethh->ether_type) == ETHERTYPE_IP ){	
 			iph =(struct ip *)packet;
-        	        inet_ntop(AF_INET,&(iph->ip_src),buf,sizeof(buf));
-	                inet_ntop(AF_INET,&(iph->ip_dst),buf2,sizeof(buf2));
-			printf("ip.sip = %s\n",buf);
-			printf("ip.dip = %s\n",buf2);
+        	        inet_ntop(AF_INET,&(iph->ip_src),sip,sizeof(sip));
+	                inet_ntop(AF_INET,&(iph->ip_dst),dip,sizeof(dip));
 			packet += (iph->ip_hl * 4);
 		
 			if(iph->ip_p == IPPROTO_TCP){
 				tcph =(struct tcphdr *)packet;
-				printf("tcp.sport = %d\n",ntohs(tcph->th_sport));
-				printf("tcp.dport = %d\n",ntohs(tcph->th_dport));
+				sport = ntohs(tcph->th_sport);
+				dport = ntohs(tcph->th_dport);
 				packet += (tcph->th_off * 4);
+				
+				if(sport == 80 || dport == 80){
+             		       	        printf("=============================\n");
+			         	printf("eth.smac = "); for(j=0;j<6;j++)printf("%02x:",ethh->ether_shost[j]); printf("\n");
+	                        	printf("eth.dmac = "); for(j=0;j<6;j++)printf("%02x:",ethh->ether_dhost[j]); printf("\n");
+                	        	printf("ip.sip = %s\n",sip);
+	                		printf("ip.dip = %s\n",dip);
+                                    	printf("tcp.sport = %d\n",sport);
+                               	        printf("tcp.dport = %d\n",dport);
 
-				printf("data:\n");
-                        	for(j=1;j<=ntohs(iph->ip_len) - (tcph->th_off * 4) - (iph->ip_hl * 4) - 14;j++){
-                        	printf("%02x ",*packet);
-                        	packet++;
+					printf("data:\n");
+                        		for(j=1;j<=ntohs(iph->ip_len) - (tcph->th_off * 4) - (iph->ip_hl * 4) - 14;j++){
+                        			printf("%02x ",*packet);
+                        			packet++;
 
-				if(j % 20 == 0)
-        	                printf("\n");
-	                        }
+						if(j % 20 == 0)
+        	               	                printf("\n");
+	                       		 }
+			       		 printf("\n=============================\n");
+				}
 			}
 		}
-                printf("\n==============================\n");
-
 	}
 	pcap_close(handle);	
 	return(0);
